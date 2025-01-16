01d32c9c69207b24266be75e75c56f6e255ad506
leejunho0622 <lee49409780@gmail.com>
2025-01-16 14:03:32 +0900

    Add ActionFactory.java

---
+++ src/main/java/controller/ActionFactory.java
@@ -0,0 +1,33 @@
+package controller;
+
+import user.action.*;
+import util.HttpMethod;
+
+public class ActionFactory {
+	private ActionFactory() {}
+	private static ActionFactory instance = new ActionFactory();
+	public static ActionFactory getInstance() {
+		return instance;
+	}
+	
+	public Action getAction(String path, String command, HttpMethod method) {
+		Action action = null;
+		
+		if(path == null || command == null)
+			return action;
+		
+		if(path.equals("users"))
+			return getUserAction(command, method);
+		
+		return action;
+	}
+	
+	private Action getUserAction(String command, HttpMethod method) {
+		Action action = null;
+		
+		if(command.equals("join") && method == HttpMethod.POST)
+			return new JoinFormAction();
+		
+		return action;
+	}
+}
