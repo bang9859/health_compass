b3606070fd1ff5cf9cbdf97bd8a3bb74295fc157
leejunho0622 <lee49409780@gmail.com>
2025-01-20 12:29:51 +0900

    Update LogoutAction

--- src/main/java/user/action/LogoutAction.java
+++ src/main/java/user/action/LogoutAction.java
@@ -1,5 +1,21 @@
 package user.action;
 
-public class LogoutAction {
+import java.io.IOException;
 
+import controller.Action;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+public class LogoutAction implements Action {
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		
+		session.removeAttribute("log");
+		session.invalidate();
+
+		response.sendRedirect("/index.jsp");
+	}
 }
