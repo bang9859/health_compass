e2b6ffe1da9dbd965e5bc1c1fc91a9ed0daaf40f
leejunho0622 <lee49409780@gmail.com>
2025-01-20 11:29:08 +0900

    Add validation-login.js

---
+++ src/main/webapp/resources/script/users/validation-login.js
@@ -0,0 +1,61 @@
+window.onload = () => {
+	const form = document.getElementById("form-login");
+
+	const username = document.getElementById("username");
+	const password = document.getElementById("password");
+
+	const px = "29.46px";
+	
+	let isPassUsername = false;
+	let isPassPassword = false;
+	
+	username.addEventListener("focusout", async e => {
+		const input = e.target.value;
+		const errId = document.getElementById("err-msg-id");
+
+		if (input === "") {
+			errId.style.display = "block";
+			errId.style.marginBottom = '15px';
+			username.style.marginBottom = '0';
+		} else {
+			isPassUsername = true;
+			errId.style.display = "none";
+			username.style.marginBottom = px;
+		}
+	});
+
+	password.addEventListener("focusout", async e => {
+		const input = e.target.value;
+		const errPw = document.getElementById("err-msg-pw");
+
+		if (input === "") {
+			errPw.style.display = "block";
+			errPw.style.marginBottom = '15px';
+			password.style.marginBottom = '0';
+		} else {
+			isPassPassword = true;
+			errPw.style.display = "none";
+			password.style.marginBottom = px;
+		}
+	});
+
+	form.addEventListener("submit", async e => {
+		e.preventDefault();
+
+		if (username.value === "") {
+			const errEmpty = document.getElementById("err-msg-id");
+			errEmpty.style.display = "block";
+			errEmpty.style.marginBottom = '15px';
+			username.style.marginBottom = "0";
+		}
+		if (password.value === "") {
+			const errEmpty = document.getElementById("err-msg-pw");
+			errEmpty.style.display = "block";
+			errEmpty.style.marginBottom = '15px';
+			password.style.marginBottom = "0";
+		}
+		if (isPassUsername && isPassPassword) {
+			form.submit();
+		}
+	});
+}
