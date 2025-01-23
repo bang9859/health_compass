09099c8267f3a23ed12987e98aa8b52a632b933e
leejunho0622 <lee49409780@gmail.com>
2025-01-23 18:24:13 +0900

    Update login action

--- src/main/java/user/action/LoginFormAction.java
+++ src/main/java/user/action/LoginFormAction.java
@@ -1,12 +1,15 @@
 package user.action;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import controller.Action;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import user.model.Bookmark;
 import user.model.User;
 import user.model.UserDao;
 
@@ -18,8 +21,16 @@ public class LoginFormAction implements Action {
 		UserDao userDao = UserDao.getInstance();
 		User user = userDao.findUserByUsername(username);
 		if (user != null && (user.checkPassword(password) || user.checkCryptPassword(password))) {
+			List<String> list = userDao.allSearchBookmark(user.getUserCode());
+			List<Bookmark> bookmarkList = new ArrayList<Bookmark>();
+			
+			SearchHospital sh = new SearchHospital();
+			for(int i=0; i<list.size(); i++) {
+				bookmarkList.add(sh.searchHospital(list.get(i)));
+			}
 			HttpSession session = request.getSession();
 			session.setAttribute("log", user);
+			session.setAttribute("bookmark", bookmarkList);
 			response.sendRedirect("/");
 			System.err.println("로그인 성공");
 		} else {
