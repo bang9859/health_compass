6ec7f6d15cdfae13545272e24eb5e47175a8b45c
bang9859 <kokozz9859@naver.com>
2025-01-31 15:54:41 +0900

    modify schedule model constructor

--- src/main/java/schedule/model/ScheduleDao.java
+++ src/main/java/schedule/model/ScheduleDao.java
@@ -30,15 +30,17 @@ public class ScheduleDao {
 	public void addSchedule(ScheduleRequestDto scheduleDto) {
 		conn = DBManager.getConnection();
 
-		String sql = "INSERT INTO schedule (user_code, medicine_code, start_date, end_date, daily_frequency) VALUES (?, ?, ?, ?, ?)";
+		String sql = "INSERT INTO schedule (user_code, medicine_code,medicine_name,deposit_method, start_date, end_date, daily_frequency) VALUES (?,?, ?, ?, ?, ?, ?)";
 
 		try {
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setInt(1, scheduleDto.getUserCode());
-			pstmt.setInt(2, scheduleDto.getMedicineCode());
-			pstmt.setString(3, scheduleDto.getStartDate());
-			pstmt.setString(4, scheduleDto.getEndDate());
-			pstmt.setInt(5, scheduleDto.getDailyFrequency());
+			pstmt.setInt(2, scheduleDto.getMedicineCode());			
+			pstmt.setString(3, scheduleDto.getMedicineName());
+			pstmt.setString(4, scheduleDto.getDepositMethod());
+			pstmt.setString(5, scheduleDto.getStartDate());
+			pstmt.setString(6, scheduleDto.getEndDate());
+			pstmt.setInt(7, scheduleDto.getDailyFrequency());
 
 			pstmt.execute();
 
@@ -70,13 +72,16 @@ public class ScheduleDao {
 	        rs = pstmt.executeQuery();
 
 	        while (rs.next()) {
-	            int userCode = rs.getInt("user_code");
-	            int medicineCode = rs.getInt("medicine_code");
-	            String startDate = rs.getString("start_date");
-	            String endDate = rs.getString("end_date");
-	            int dailyFrequency = rs.getInt("daily_frequency");
+	        	int scheduleCode = rs.getInt(1);
+	            int userCode = rs.getInt(2);
+	            int medicineCode = rs.getInt(3);	            
+	            String medicineName = rs.getString(4);
+	            String depositMethod = rs.getString(5);
+	            String startDate = rs.getString(6);
+	            String endDate = rs.getString(7);
+	            int dailyFrequency = rs.getInt(8);
 
-	            Schedule schedule = new Schedule(userCode, medicineCode, startDate, endDate, dailyFrequency);
+	            Schedule schedule = new Schedule(scheduleCode,userCode, medicineCode,medicineName,depositMethod, startDate, endDate, dailyFrequency);
 	            schedules.add(schedule);
 	        }
 	    } catch (SQLException e) {
@@ -92,6 +97,45 @@ public class ScheduleDao {
 	    }
 	    System.out.println(schedules);
 	    return schedules;
+	}
+	
+	public Schedule findScheduleByScheduleCode(int scheduleCode) {
+		Schedule schedule = null;
+		
+		conn = DBManager.getConnection();
+
+		String sql = "SELECT * FROM schedule WHERE schedule_code=?";
+
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, scheduleCode);
+			rs = pstmt.executeQuery();
+			
+			if (rs.next()) {
+				
+	            int userCode = rs.getInt(2);
+	            int medicineCode = rs.getInt(3);	            
+	            String medicineName = rs.getString(4);
+	            String depositMethod = rs.getString(5);
+	            String startDate = rs.getString(6);
+	            String endDate = rs.getString(7);
+	            int dailyFrequency = rs.getInt(8);
+
+	            schedule = new Schedule(scheduleCode,userCode, medicineCode,medicineName,depositMethod, startDate, endDate, dailyFrequency);
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				conn.close();
+				pstmt.close();
+				rs.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return schedule;
 	}
 
 }
